ba3d6d82bfffa40b8aadbb3d18e41be81f61f162
Martin Lasek <martinlasek@hotmail.de>
2018-03-02 14:17:26 +0100

    Remove mysql as a dependency

--- Package.swift
+++ Package.swift
@@ -6,7 +6,6 @@ let package = Package(
         // Vapor
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2),
         .Package(url: "https://github.com/vapor/leaf-provider.git", majorVersion: 1),
-        .Package(url: "https://github.com/vapor/mysql-provider.git", majorVersion: 2),
         .Package(url: "https://github.com/vapor/auth-provider.git", majorVersion: 1),
 
         // Nodes
